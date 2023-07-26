0bf3935588cbb5e7de53f088f02dc11d0594eb69
= <marcoron00@gmail.com>
2023-07-26 12:53:21 +0200

    Fixed ComputePi test

--- ComputePi/test_generator.py
+++ ComputePi/test_generator.py
@@ -3,17 +3,18 @@ import sys
 
 template = "\n\t<testbench\n\tPd5=\"iters_\"\n\tPd6=\"res_\"\n\titers=\"iters_\"\n\tres=\"res_\"/>"
 
-iterations_upper_bound = 2**20
+iterations_lower_bound = 2**16
+iterations_upper_bound = 2**16
 
-# upper bound is included
-def gen(max_val):
-    value = 2
-    while value < max_val:
+# upper and lower bounds are included
+def gen(min_val, max_val):
+    value = min_val
+    while value <= max_val:
         yield value, 0
         value *= 2
 
-def generate_values(max_val):
-    g = gen(max_val)
+def generate_values(min_val, max_val):
+    g = gen(min_val, max_val)
     while True:
         try:
             iters, result = next(g)
@@ -23,7 +24,7 @@ def generate_values(max_val):
 
 result = "<?xml version=\"1.0\"?>\n<function>"
 
-for entry in generate_values(iterations_upper_bound):
+for entry in generate_values(iterations_lower_bound, iterations_upper_bound):
     result += entry
 
 result += "\n</function>"
